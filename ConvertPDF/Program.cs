0fd2da621b926ce112d963b2169f7d4f6cc60b62
Sergio De Luca <serg.r.deluca@gmail.com>
2024-07-11 15:37:34 -0300

    deshabilitacion de antiforgerytoken

--- ConvertPDF/Program.cs
+++ ConvertPDF/Program.cs
@@ -1,7 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddControllersWithViews(options =>
+{
+    options.Filters.Add(new IgnoreAntiforgeryTokenAttribute());
+});
+
 
 var app = builder.Build();
 
